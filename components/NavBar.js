00046dc0c57b2f9670d9c03772c1e5edf8a99276
fullpistoladev <mateus_henrique2011@outlook.com>
2022-02-02 20:53:55 -0300

    navlinks hover

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,45 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react';
 import styles from '../styles/Home.module.css'
 import { motion } from "framer-motion" 
 
 const NavBar = () => {
+
+  const [jaguaracambe, setJaguaracambe] = useState(false);
+  const [projetos, setProjetos] = useState(false);
+  const [especies, setEspecies] = useState(false);
+  const [apoie, setApoie] = useState(false);
+
   return (
     <div className={styles.navbar}>
           <img className={styles.logo} src="/logo.jpeg" alt="jaguaracambe logo"/>
-        
           <nav>
             <div className={styles.links}>
-                <a className={styles.link} href="">JAGUARACAMBÉ</a>
-                <div className={styles.sublinks}>
-                  <a className={styles.sublink} href="">Quem somos</a>
-                  <a className={styles.sublink} href="">Missao e valores</a>
-                  <a className={styles.sublink} href="">Equipe</a>
-                </div>
+                <a className={styles.link} href=""
+                  onMouseEnter={() => setJaguaracambe(true)}
+                  onMouseLeave={() => setJaguaracambe(false)}
+                  >JAGUARACAMBÉ</a>
+                  { jaguaracambe && <ul  className={styles.sublinks}>
+                    <a className={styles.sublink} href="">Quem somos</a>
+                    <a className={styles.sublink} href="">Missao</a>
+                    <a className={styles.sublink} href="">Equipe</a>
+                  </ul>}
             </div>
             <div className={styles.links}>
-                <a className={styles.link} href="">PROJETOS</a>
-                <div className={styles.sublinks}>
+                <a className={styles.link} href=""
+                   onMouseEnter={() => setProjetos(true)}
+                   onMouseLeave={() => setProjetos(false)}
+                >PROJETOS</a>
+                { projetos && <ul className={styles.sublinks}>
                   <a className={styles.sublink} href="">Educação Ambiental</a>
                   <a className={styles.sublink} href="">Pesq. Científica</a>
                   <a className={styles.sublink} href="">Sou EU Pequi</a>
-                </div>
+                </ul>}
             </div>
             <div className={styles.links}>
-                <a className={styles.link} href="">ESPÉCIES</a>
-                <div className={styles.sublinks}>
+                <a className={styles.link} href=""
+                   onMouseEnter={() => setEspecies(true)}
+                   onMouseLeave={() => setEspecies(false)}
+                >ESPÉCIES</a>
+                { especies && <ul className={styles.sublinks}>
                   <a className={styles.sublink} href="">Canídeos</a>
                   <a className={styles.sublink} href="">Felídeos</a>
                   <a className={styles.sublink}href="">Procionidae</a>
                   <a className={styles.sublink}href="">Mephitidae</a>
                   <a className={styles.sublink}href="">Mustelidae</a>
-                </div>
+                </ul>}
             </div>
             <div className={styles.links}>
-                <a className={styles.link} href="">APOIE</a>
-                <div className={styles.sublinks}>
+                <a className={styles.link} href=""
+                   onMouseEnter={() => setApoie(true)}
+                   onMouseLeave={() => setApoie(false)}
+                >APOIE</a>
+                { apoie && <ul className={styles.sublinks}>
                   <a className={styles.sublink} href="">Crowfounding</a>
                   <a className={styles.sublink} href="">Empresa Parceira</a>
-                </div>
+                </ul>}
             </div>
           </nav>
         </div>
